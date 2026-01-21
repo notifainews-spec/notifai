cc1dd68806f82e5be7a672d5475f58282b3632c4
notifainews-spec <notifainews@gmail.com>
2026-01-21 17:16:04 +0500

    Update server.js package with rewards features

--- server.js
+++ server.js
@@ -689,7 +689,7 @@ const MAX_SECONDS_PER_WEEK = 7 * 24 * 60 * 60;
 const MAX_SECONDS_PER_CALL = 6 * 60 * 60;
 const MIN_MS_BETWEEN_CALLS = 120000;
 
-const USER_CACHE = new Map();
+// USER_CACHE is declared later in the file
 const REFERRAL_CACHE = new Map();
 const USER_CACHE_TTL = 30000;
 const REFERRAL_CACHE_TTL = 60000;
